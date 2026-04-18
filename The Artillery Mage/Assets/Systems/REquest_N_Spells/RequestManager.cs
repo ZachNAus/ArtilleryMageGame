d92b7aca43389a636489a6e7a5d2624864b5ac6f
Zachary N <taxi.man981@gmail.com>
2026-04-18 14:17:07 +0930

    Requests: Now picks

---
+++ The Artillery Mage/Assets/Systems/REquest_N_Spells/RequestManager.cs
@@ -0,0 +1,125 @@
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RequestManager : MonoBehaviour
+{
+	public static RequestManager Instance { get; private set; }
+
+	[SerializeField] List<RequestData> requestPool;
+
+	[SerializeField] float startGameDelay = 12;
+
+	[SerializeField] Vector2 requestPopupTime = new Vector2(20, 35);
+
+	[ReadOnly]
+	public List<ActiveRequestData> currentlyActiveRequests = new List<ActiveRequestData>();
+
+	public event Action<RequestData> OnRequestAdded;
+
+	public event Action<RequestData> OnRequestRemoved;
+
+	public event Action<RequestData> OnRequestExpired;
+	public event Action<RequestData> OnRequestFailed;
+	public event Action<RequestData> OnRequestPassed;
+
+	[System.Serializable]
+	public class ActiveRequestData
+	{
+		public RequestData request;
+
+		public float timeLeft;
+	}
+
+	float timeTillNextSpawn;
+	private void Awake()
+	{
+		Instance = this;
+
+		timeTillNextSpawn = startGameDelay;
+	}
+
+	public void SpawnRequest()
+	{
+		var pool = new List<RequestData>();
+		pool.AddRange(requestPool.Where(x => currentlyActiveRequests.All(y => y.request.desiredLocation != x.desiredLocation)));
+
+		var rand = pool.GetRandom();
+
+		ActiveRequestData r = new ActiveRequestData()
+		{
+			request = rand,
+			timeLeft = rand.timeToComplete
+		};
+
+		currentlyActiveRequests.Add(r);
+		OnRequestAdded?.Invoke(rand);
+
+		timeTillNextSpawn = UnityEngine.Random.Range(requestPopupTime.x, requestPopupTime.y);
+	}
+
+	private void Update()
+	{
+		timeTillNextSpawn -= Time.deltaTime;
+
+		if (timeTillNextSpawn <= 0)
+			SpawnRequest();
+
+		var cleanup = new List<ActiveRequestData>();
+		foreach (var r in currentlyActiveRequests)
+		{
+			r.timeLeft -= Time.deltaTime;
+
+			if (r.timeLeft <= 0)
+			{
+				cleanup.Add(r);
+			}
+		}
+
+		foreach (var c in cleanup)
+		{
+			currentlyActiveRequests.Remove(c);
+
+			OnRequestExpired?.Invoke(c.request);
+			OnRequestRemoved?.Invoke(c.request);
+		}
+	}
+
+	void OnSpellCasted(SpellData spell, LocationList location)
+	{
+		ActiveRequestData cleanup = null;
+		bool fuckedUp = false;
+
+		foreach (var r in currentlyActiveRequests)
+		{
+			if(r.request.desiredLocation == location)
+			{
+				if (r.request.spellsThatWork.Contains(spell))
+				{
+					cleanup = r;
+					break;
+				}
+				else if (r.request.instantFailSpells.Contains(spell))
+				{
+					cleanup = r;
+					fuckedUp = true;
+					break;
+				}
+			}
+		}
+
+		if (cleanup != null)
+		{
+			currentlyActiveRequests.Remove(cleanup);
+			if (fuckedUp)
+				OnRequestFailed?.Invoke(cleanup.request);
+			else
+				OnRequestPassed?.Invoke(cleanup.request);
+		
+			OnRequestRemoved?.Invoke(cleanup.request);
+		}
+	}
+}
