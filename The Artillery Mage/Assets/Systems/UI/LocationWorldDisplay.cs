306fbd3cf493e8b5d237c06c1c299f7e9756a23c
Zachary N <taxi.man981@gmail.com>
2026-04-19 11:31:23 +0930

    VFX setup:

--- The Artillery Mage/Assets/Systems/UI/LocationWorldDisplay.cs
+++ The Artillery Mage/Assets/Systems/UI/LocationWorldDisplay.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class LocationWorldDisplay : MonoBehaviour
 {
 	[SerializeField] LocationList location;
 
     [SerializeField] TextMeshProUGUI liberationText;
+
+	[SerializeField] SpellsToVFX vfxSetup = new SpellsToVFX();
+
+	[System.Serializable]
+	public class SpellsToVFX : SerializableDictionary<SpellData, VFXInfo> { }
+
+	[System.Serializable]
+	public class VFXInfo
+	{
+		public List<VisualEffect> effectsToEnable;
+	}
 
 	private void Update()
 	{
